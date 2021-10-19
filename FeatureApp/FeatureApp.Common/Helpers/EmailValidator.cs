c284f71729b74c088583a27366244b5e39d6e963
Baskaran Govindaras <saibaskar57@hotmail.com>
2021-10-19 11:36:28 +0800

    Added documentation for EmailValidator

--- FeatureApp/FeatureApp.Common/Helpers/EmailValidator.cs
+++ FeatureApp/FeatureApp.Common/Helpers/EmailValidator.cs
@@ -1,9 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace FeatureApp.Common.Helpers
+﻿namespace FeatureApp.Common.Helpers
 {
+    using System.ComponentModel.DataAnnotations;
+
     public static class EmailValidator
     {
+        /// <summary>
+        /// Validate email format.
+        /// </summary>
+        /// <param name="email">The email to be validated.</param>
+        /// <returns>The validated email.</returns>
         public static bool IsValid(string email)
         {
             return new EmailAddressAttribute().IsValid(email);
